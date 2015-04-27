c670a7c6ba03a1963da68419a1df486908d68a63
Troy Holland <tgh9@georgetown.edu>
2015-04-27 16:26:20 -0400

    ui updates

--- Q3Screen.java
+++ Q3Screen.java
@@ -22,7 +22,16 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
+//import Q2Screen.openQuizButtonListener;
+
+
+
+//import Q2Screen.openQuizButtonListener;
+
+
+//import Q2Screen.openQuizButtonListener;
 
 import java.awt.Font;
 import java.awt.Color;
@@ -33,18 +42,23 @@ import java.awt.event.ActionEvent;
 
 public class Q3Screen extends JPanel {
 
-	private JPanel q3screen;
-	private JPanel q3screenb;
+	private JPanel questionArea;
+	private JPanel submitArea;
+	private QuestionPanel genericPanel;
 	
 	
 	private AppletMain applet;
 	
-	String[][] question = new String[][]{{"In the last century, the global average temperature did what?", "C", "A. Did not change", "B. Increased by less than 1°F", "C. Increased by more than 1°F"}};
+	String[][] question = new String[][]{{"In the last century, the global average temperature did what?"
+		, "C", "A. Did not change", "B. Increased by less than 1°F", "C. Increased by more than 1°F"}};
 	
-	String funFact = "The global average temperature increased by more than 1.4°F over the last century. In fact, according to the National Oceanic and Atmospheric Administration (NOAA), the decade from 2000 to 2010 was the warmest on record, and 2010 was tied with 2005 as the warmest year on record.";
+	String funFact = "The global average temperature increased "
+			+ "by more than 1.4°F over the last century. In fact, "
+			+ "according to the National Oceanic and Atmospheric Administration "
+			+ "(NOAA), the decade from 2000 to 2010 was the warmest on record, "
+			+ "and 2010 was tied with 2005 as the warmest year on record.";
 	
 	private int points;
-	
 	
 	class openQuizButtonListener implements ActionListener
 	{
@@ -55,69 +69,87 @@ public class Q3Screen extends JPanel {
 	}
 	
 	
-	public Q3Screen(AppletMain appletParameter, int setAsPoints)
+	public Q3Screen(AppletMain appletParameter, int setAsPoints, QuestionPanel basePanel)
 	{
 		points = setAsPoints;
-		
 		applet = appletParameter;
-		applet = appletParameter;
-
-		q3screen = new JPanel();
-		q3screen.setBackground(Color.WHITE);
-		q3screen.setLayout(new BorderLayout());
+		genericPanel = basePanel;
 		
-		q3screenb = new JPanel();
-		q3screenb.setBackground(Color.WHITE);
-		q3screenb.setLayout(new BorderLayout());
-
-
-		JLabel titleSummary = new JLabel("<html>Let's see what you know!</html>");
-		titleSummary.setFont(new Font("Helvetica", Font.BOLD, 40));
+		setUpScreen();
+	}
+	
+	public void setUpScreen()
+	{
+		setLayout(new BorderLayout());
+        
+		// edit based on question
+		genericPanel.questionText.setText(question[0][0] + "\n" + question[0][2] + 
+				"\n" +  question[0][3] + "\n" + question[0][4]);
+		
+		JButton submitButton = new JButton("Submit!");
+		submitButton.setFont(new Font("Courier", Font.PLAIN, 14));
+		submitButton.addActionListener(new openQuizButtonListener());
+		genericPanel.qButtonPanel.removeAll();
+		genericPanel.qButtonPanel.add(submitButton);
+		
+        add(genericPanel);
+	}
+	
+	/*public void setUpScreen()
+	{
+		setLayout(new BorderLayout());
+		
+		// title
+		JLabel titleSummary = new JLabel("<html><br>Let's See What You Know!</html>");
+		titleSummary.setFont(new Font("Courier", Font.PLAIN, 30));
 		titleSummary.setForeground(Color.BLACK);
 		titleSummary.setHorizontalAlignment(JLabel.CENTER);
 		titleSummary.setVerticalAlignment(JLabel.CENTER);
+		
+		// holds question
+		questionArea = new JPanel();
+		questionArea.setBackground(Color.WHITE);
+		questionArea.setOpaque(false);
+		questionArea.setLayout(new BorderLayout());
 
+		// holds submission
+		submitArea = new JPanel();
+		submitArea.setOpaque(false);
 
-
-		//  button to progress to next question
+		//  button to submit to next question
 		JPanel q2ButtonPanel = new JPanel();
-		q2ButtonPanel.setBackground(Color.WHITE);
+		q2ButtonPanel.setOpaque(false);
 		JButton q2Button = new JButton("Submit!");
-		q2Button.setFont(new Font("Helvetica", Font.BOLD, 16));
+		q2Button.setFont(new Font("Courier", Font.PLAIN, 14));
 		q2Button.addActionListener(new openQuizButtonListener());
 		q2ButtonPanel.add(q2Button);
 
-
-		JTextArea q = new JTextArea();
-		q.setText(question[0][0] + "\n" + question[0][2] + "\n" +  question[0][3] + "\n" + question[0][4]);
-		q.setWrapStyleWord(true);
-		q.setFont(new Font("Helvetica", Font.BOLD, 18));
-		q.setEditable(false);
-		JScrollPane scroll = new JScrollPane(q);
-		titleSummary.setHorizontalAlignment(JLabel.CENTER);
-		titleSummary.setVerticalAlignment(JLabel.CENTER);
+		// display question
+		JTextArea questionText = new JTextArea();
+		questionText.setText(question[0][0] + "\n" + question[0][2] + "\n" +  question[0][3] + "\n" + question[0][4]);
+		questionText.setWrapStyleWord(true);
+		questionText.setLineWrap(true);
+		questionText.setFont(new Font("Courier", Font.BOLD, 18));
+		questionText.setEditable(false);
+		questionText.setBorder(new LineBorder(Color.BLACK, 2, true));
 		
-
-		JTextField field = new JTextField(10);
+		// where info is input
+		JTextField field = new JTextField("", 5);
 		field.setHorizontalAlignment(JTextField.RIGHT);
+		field.setBorder(new LineBorder(Color.BLACK, 2, true));
 		
 		String input = new String();
 		input = field.getText();
 
-		q3screen.add(titleSummary, BorderLayout.NORTH);
-		q3screen.add(scroll, BorderLayout.CENTER);
-		q3screenb.add(field, BorderLayout.CENTER);
-		q3screenb.add(q2ButtonPanel, BorderLayout.SOUTH);
+		// add to layouts
+		questionArea.add(titleSummary, BorderLayout.NORTH);
+		questionArea.add(questionText, BorderLayout.CENTER);
+		submitArea.add(field);
+		submitArea.add(q2ButtonPanel);
 	
-
-		add(q3screen);
-		add(q3screenb);
-
-		validate();
-		repaint();
-
-		
-	}
+		add(questionArea, BorderLayout.PAGE_START);
+		add(submitArea, BorderLayout.PAGE_END);
+	}*/
 	
 	
 	// need a function(s) to display the question, display the answer choices, and update points based on the
