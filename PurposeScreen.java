b2be959790e2182188c5a45592b957a6b2b5e839
Troy Holland <tgh9@georgetown.edu>
2015-04-27 16:24:00 -0400

    ui updates

--- PurposeScreen.java
+++ PurposeScreen.java
@@ -4,9 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -14,23 +16,76 @@ import javax.swing.JLabel;
 
 //import FinalProject.HomeScreen.openQuizButtonListener;
 
+
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class PurposeScreen extends JPanel{
-
-	private JPanel purposePage;
-
 	private AppletMain applet;
 
+	// panels and buttons
 	private JPanel openHomeButtonPanel;
-
 	private JButton openHomeButton;
+	
+	// background
+	private Image backgroundImageEarth;
+	private Image scaledBackgroundImageEarth;
 
+	public PurposeScreen(AppletMain appletParameter)
+	{
+		applet = new AppletMain();
+		applet = appletParameter;
+		
+		// create background image
+        ImageIcon myBackgroundImageEarth = new ImageIcon(this.getClass().getResource("/leavesBackground.jpg"));
+        backgroundImageEarth = myBackgroundImageEarth.getImage();
+        scaledBackgroundImageEarth = backgroundImageEarth.getScaledInstance(720, 520, Image.SCALE_FAST);
+        
+        setUpScreen();
+	}
+
+	public void setUpScreen()
+	{
+		// screen layout
+		setLayout(new BorderLayout());
+		setOpaque(false);
+
+		// title
+		JLabel titleLabel = new JLabel("<html><br><br>Protecting the World around Us</html>");
+		titleLabel.setFont(new Font("Papyrus", Font.BOLD, 44));
+		titleLabel.setForeground(Color.WHITE);
+		titleLabel.setHorizontalAlignment(JLabel.CENTER);
+		
+		// intro info
+		JLabel purposeLabel = new JLabel("<html>-- Let's learn about climate change!<br><br>"
+				+ "-- How do our actions affect the Earth?<br><br>"
+				+ "-- Can we do anything to fight the consequences <br>of pollution?<br><br>"
+				+ "-- Should we even care about the environment?</html>");
+		purposeLabel.setFont(new Font("Courier", Font.PLAIN, 20));
+		purposeLabel.setForeground(Color.WHITE);
+		purposeLabel.setHorizontalAlignment(JLabel.CENTER);
+
+		//  button to progress to game
+		openHomeButtonPanel = new JPanel();
+		openHomeButtonPanel.setOpaque(false);
+		openHomeButton = new JButton("What're We Doing Then?");
+		openHomeButton.setFont(new Font("Courier", Font.PLAIN, 14));
+		openHomeButton.setBackground(Color.BLACK);
+		openHomeButton.addActionListener(new openQuizButtonListener());
+		openHomeButtonPanel.add(openHomeButton);
+
+		// add to screen
+		add(titleLabel, BorderLayout.PAGE_START);
+		add(purposeLabel, BorderLayout.CENTER);
+		add(openHomeButtonPanel, BorderLayout.PAGE_END);
+	}
+	
 	class openQuizButtonListener implements ActionListener
 	{
 		public void actionPerformed(ActionEvent event)
@@ -38,51 +93,15 @@ public class PurposeScreen extends JPanel{
 			applet.purposeToHome();
 		}
 	}
-
-
-	public PurposeScreen(AppletMain appletParameter)
+	
+	/*
+	 * for background
+	 */
+	@Override
+	protected void paintComponent(Graphics background) 
 	{
-
-		applet = new AppletMain();
-		applet = appletParameter;
-
-		purposePage = new JPanel();
-		purposePage.setBackground(Color.WHITE);
-		purposePage.setLayout(new BorderLayout());
-
-		JLabel titleSummary = new JLabel("<html><head><title>For Educators: </title><head>"
-				+ "<body><p></p><p></p>"
-				+ "<p>For Educators: </p>"
-				+ "<p>The purpose of this game is to teach middle</p>"
-				+ "<p>school students about climate change and how</p>"
-				+ "<p>their own actions contribute to the earth's</p> "
-				+ "<p>overall global warming. We hope that this game</p> "
-				+ "<p>encourages teachers to continue to teach about</p>"
-				+ "<p>global warming, as well as inform students about</p>"
-				+ "<p>what actions that they can take to help reduce</p>"
-				+ "<p>CO2 emissions and global warming.</p></body></html>");
-		titleSummary.setFont(new Font("Helvetica", Font.BOLD, 28));
-		titleSummary.setForeground(Color.BLACK);
-		titleSummary.setHorizontalAlignment(JLabel.CENTER);
-		titleSummary.setVerticalAlignment(JLabel.CENTER);
-
-		//  button to progress to game
-		openHomeButtonPanel = new JPanel();
-		openHomeButtonPanel.setBackground(Color.WHITE);
-		openHomeButton = new JButton("Start!");
-		openHomeButton.setFont(new Font("Helvetica", Font.BOLD, 16));
-		openHomeButton.addActionListener(new openQuizButtonListener());
-		openHomeButtonPanel.add(openHomeButton);
-
-		purposePage.add(titleSummary, BorderLayout.CENTER);
-		purposePage.add(openHomeButtonPanel, BorderLayout.SOUTH);
-
-		add(purposePage);
-
-		validate();
-		repaint();
-
-
+	    super.paintComponent(background);
+	    background.drawImage(scaledBackgroundImageEarth, 0, 0, this);
 	}
 
 }
