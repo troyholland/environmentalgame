a8332a4938e8255ed248e02401c36fab697c1b09
Troy Holland <tgh9@georgetown.edu>
2015-04-27 16:25:41 -0400

    ui updates

--- Q2Screen.java
+++ Q2Screen.java
@@ -5,6 +5,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,26 +24,48 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+//import Q1Screen.openQuizButtonListener;
+
+import javax.swing.border.LineBorder;
+
+//import Q3Screen.openQuizButtonListener;
+
+
+//import Q1Screen.openQuizButtonListener;
+
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class Q2Screen extends JPanel implements QInterface {
 
-	private JPanel q2screen;
-	private JPanel q2screenb;
+	private JPanel questionArea;
+	private JPanel submitArea;
+	private QuestionPanel genericPanel;
 	
 	private AppletMain applet;
 	
-	String[][] question = new String[][]{{"What percentage of climate experts believe humans are causing climate change?", "C", "A. 25%", "B. 50%", "C. 97%"}};
+	String[][] question = new String[][]{{"What percentage of climate experts "
+			+ "believe humans are causing climate change?", "C", "A. 25%", "B. 50%", "C. 97%"}};
 	
-	String funFact = "In the field of climate studies, a survey of all peer-reviewed papers on the subject 'global climate change' published between 1993 and 2003 shows that not a single paper rejected the consensus position that global warming is man-made. The major scientific agencies of the United States — including the National Aeronautics and Space Administration (NASA) and the National Oceanic and Atmospheric Administration (NOAA) — agree that climate change is occurring and that humans are contributing to it. In 2010, the National Research Council concluded that 'Climate change is occurring, is very likely caused by human activities, and poses significant risks for a broad range of human and natural systems'";
+	String funFact = "In the field of climate studies, a survey of all "
+			+ "peer-reviewed papers on the subject 'global climate change'"
+			+ " published between 1993 and 2003 shows that not a single paper"
+			+ " rejected the consensus position that global warming is man-made. "
+			+ "The major scientific agencies of the United States — including the"
+			+ " National Aeronautics and Space Administration (NASA) and the "
+			+ "National Oceanic and Atmospheric Administration (NOAA) — agree "
+			+ "that climate change is occurring and that humans are contributing "
+			+ "to it. In 2010, the National Research Council concluded that "
+			+ "'Climate change is occurring, is very likely caused by human activities, "
+			+ "and poses significant risks for a broad range of human and natural systems'";
 	
 	private int points;
-	
 	
 	class openQuizButtonListener implements ActionListener
 	{
@@ -53,69 +76,32 @@ public class Q2Screen extends JPanel implements QInterface {
 	}
 	
 	
-	public Q2Screen(AppletMain appletParameter, int setAsPoints)
+	public Q2Screen(AppletMain appletParameter, int setAsPoints, QuestionPanel basePanel)
 	{
 		points = setAsPoints;
 		applet = appletParameter;
-
-		q2screen = new JPanel();
-		q2screen.setBackground(Color.WHITE);
-		q2screen.setLayout(new BorderLayout());
-		
-		q2screenb = new JPanel();
-		q2screenb.setBackground(Color.WHITE);
-		q2screenb.setLayout(new BorderLayout());
-
-
-		JLabel titleSummary = new JLabel("<html>Let's see what you know!</html>");
-		titleSummary.setFont(new Font("Helvetica", Font.BOLD, 32));
-		titleSummary.setForeground(Color.BLACK);
-		titleSummary.setHorizontalAlignment(JLabel.CENTER);
-		titleSummary.setVerticalAlignment(JLabel.CENTER);
-
-
-
-		//  button to progress to next question
-		JPanel q2ButtonPanel = new JPanel();
-		q2ButtonPanel.setBackground(Color.WHITE);
-		JButton q2Button = new JButton("Submit!");
-		q2Button.setFont(new Font("Helvetica", Font.BOLD, 16));
-		q2Button.addActionListener(new openQuizButtonListener());
-		q2ButtonPanel.add(q2Button);
-
-
-		JTextArea q = new JTextArea();
-		q.setText(question[0][0] + "\n" + question[0][2] + "\n" +  question[0][3] + "\n" + question[0][4]);
-		q.setWrapStyleWord(true);
-		q.setFont(new Font("Helvetica", Font.BOLD, 18));
-		q.setEditable(false);
-		JScrollPane scroll = new JScrollPane(q);
-		titleSummary.setHorizontalAlignment(JLabel.CENTER);
-		titleSummary.setVerticalAlignment(JLabel.CENTER);
-		
-
-		JTextField field = new JTextField(10);
-		field.setHorizontalAlignment(JTextField.RIGHT);
-		
-		String input = new String();
-		input = field.getText();
-
-		q2screen.add(titleSummary, BorderLayout.NORTH);
-		q2screen.add(scroll, BorderLayout.CENTER);
-		q2screenb.add(field, BorderLayout.CENTER);
-		q2screenb.add(q2ButtonPanel, BorderLayout.SOUTH);
-	
-
-		add(q2screen);
-		add(q2screenb);
-
-		validate();
-		repaint();
-
-
-		
+		genericPanel = basePanel;
+        
+		setUpScreen();
 	}
 	
+	public void setUpScreen()
+	{
+		setLayout(new BorderLayout());
+        
+		// edit based on question
+		genericPanel.questionText.setText(question[0][0] + "\n" + question[0][2] + 
+				"\n" +  question[0][3] + "\n" + question[0][4]);
+		
+		JButton submitButton = new JButton("Submit!");
+		submitButton.setFont(new Font("Courier", Font.PLAIN, 14));
+		submitButton.addActionListener(new openQuizButtonListener());
+		genericPanel.qButtonPanel.removeAll();
+		genericPanel.qButtonPanel.add(submitButton);
+		
+        add(genericPanel);
+	}
+
 	
 	// need a function(s) to display the question, display the answer choices, and update points based on the
 	// user's answer
@@ -154,5 +140,4 @@ public class Q2Screen extends JPanel implements QInterface {
 		// TODO Auto-generated method stub
 		
 	};
-	
 }
